dfda0cc2a4620d61c6aaf989885bfcd1222b3b08
Kareem Atef <143643235+KareemAtefEECE@users.noreply.github.com>
2025-09-09 17:04:36 +0300

    Update RV32I_tb.v

--- RV32I_tb.v
+++ RV32I_tb.v
@@ -4,20 +4,19 @@
 
 module RV32I_tb();
 
-reg clk,rst;
+reg fun_clk,scan_clk,fun_rst,scan_rst,test_mode;
 
-RV32I DUT(clk,rst);
+RV32I DUT(fun_clk,scan_clk,fun_rst,scan_rst,test_mode);
 
 initial begin
-	clk=0;
+	fun_clk=0;
 	forever
-	#5 clk=~clk;
+	#5 fun_clk=~fun_clk;
 end
-
+/*
 initial $readmemh("inst_memory.mem",DUT.Inst_Mem.inst_mem);
 initial $readmemh("data_memory.mem",DUT.DAT_MEM.data_mem);
 initial $readmemh("reg_file_memory.mem",DUT.RF.reg_file);
-
 
 initial begin
 	rst=1;
@@ -29,5 +28,15 @@ initial begin
 	#400
 	$stop;
 end
+*/
+	initial begin
+	fun_rst=1;
+	test_mode = 0;
+	#10
+	fun_rst=0;
+	#400
+	$stop;
+end
 
 endmodule
+
