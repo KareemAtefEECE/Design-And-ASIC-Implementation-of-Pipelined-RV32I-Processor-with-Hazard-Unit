ded0b2d0dcd57aba3024c73249a0bfcd0ffcda81
Kareem Atef <143643235+KareemAtefEECE@users.noreply.github.com>
2025-09-09 17:01:31 +0300

    Update register_file.v

--- Datapath/register_file.v
+++ Datapath/register_file.v
@@ -11,18 +11,21 @@ input[4:0] A1,A2,A3;
 input[31:0] WD3;
 output[31:0] RD1,RD2;
 
-reg[31:0] reg_file[0:31];
+reg[31:0] reg_file[0:15];
 
 integer i;
 
-always @(negedge clk or posedge rst) begin
-	if (rst) begin
-		reg_file[0]<=0;
-	end
-	else if (WE) reg_file[A3]<=WD3;
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        for (i = 0; i < 16; i = i + 1)
+            reg_file[i] <= 32'd0;
+    end else if (WE) begin
+        reg_file[A3] <= WD3;
+    end
 end
 
-assign RD1 = reg_file[A1];
-assign RD2 = reg_file[A2];
+// Read ports with write-through (avoid RAW hazards)
+assign RD1 = (WE && (A1 != 0) && (A1 == A3)) ? WD3 : reg_file[A1];
+assign RD2 = (WE && (A2 != 0) && (A2 == A3)) ? WD3 : reg_file[A2];
 
 endmodule
